5e45faf745dec57140e874582466e24414d2b1eb
williamwangwei <2840769859@qq.com>
2016-08-03 21:15:30 +0800

    提交

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -5,6 +5,8 @@ public class Test01 {
 	
 	@Test
 	public void tt(){
-		System.out.println("123");
+		System.out.println("maxMemory============="+ Runtime.getRuntime().maxMemory());
+		System.out.println("freeMemory============"+ Runtime.getRuntime().freeMemory());
+		System.out.println("totalMemory==========="+Runtime.getRuntime().totalMemory());
 	}
 }
